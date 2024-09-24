f071853a89976d72b5c996f0c0b1430f8d03d735
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-09-24 15:49:42 +0300

    Start test for post |category

--- framework/symfony/tests/Controller/CategoryControllerTest.php
+++ framework/symfony/tests/Controller/CategoryControllerTest.php
@@ -2,16 +2,35 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CategoryControllerTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $repository;
+    private string $path = '/category/';
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->repository = $this->manager->getRepository(Category::class);
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+        $this->manager->flush();
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Category index');
     }
 }
--- framework/symfony/tests/Controller/PostControllerTest.php
+++ framework/symfony/tests/Controller/PostControllerTest.php
@@ -2,16 +2,45 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\Form\Test\TypeTestCase;
 
-class PostControllerTest extends WebTestCase
+class PostControllerTest extends TypeTestCase
 {
-    public function testSomething(): void
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+    private string $path = '/post/';
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+    public function testCreatePost()
+    {
+        $formData = array(
+            'title' => 'Test Post',
+            'description' => 'Description of test post'
+        );
+
+        $objectToCompare = new Post();
+        // $objectToCompare will retrieve data from the form submission; pass it as the second argument
+        $form = $this->factory->create(PostType::class, $objectToCompare);
+
+        $object = new Post();
+        $object->setTitle($formData['title']);
+        $object->setDescription($formData['description']);
+
+        // submit the data to the form directly
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        // check that $objectToCompare was modified as expected when the form was submitted
+        $this->assertEquals($object, $objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
     }
 }
